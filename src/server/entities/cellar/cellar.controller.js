a2927a168df5557b867876b75906ecef61a5013d
Hackjack <jack03113@gmail.com>
2021-06-17 02:09:00 +0200

    Add API routes for Cellar (getById, listAll)

--- src/server/entities/cellar/cellar.controller.js
+++ src/server/entities/cellar/cellar.controller.js
@@ -63,4 +63,46 @@ export default class CellarController {
             return res.status(500).json({ error: e });
         }
     }
+
+    static async getById(req, res) {
+        try {
+            const { userId } = req.session;
+            const { id } = req.params;
+
+            const cellar = await Cellar.findOne({ $or: [{ creator: userId }, { owners: userId }], _id: id })
+                .populate({ path: 'bottles' })
+                .lean({ virtuals: true });
+
+            if (!cellar) {
+                return res.status(404).json({ error: { message: 'Cellar not found' } });
+            }
+
+            return res.json(cellar);
+        } catch (e) {
+            console.error(e);
+            return res.status(500).json({ error: e });
+        }
+    }
+
+    static async getAll(req, res) {
+        try {
+            const { userId } = req.session;
+
+            const cellar = await Cellar.find({ $or: [{ creator: userId }, { owners: userId }] })
+                .select('name')
+                .populate('creator', 'name')
+                .lean({
+                    virtuals: true,
+                });
+
+            if (!cellar) {
+                return res.status(404).json({ error: { message: 'Cellar not found' } });
+            }
+
+            return res.json(cellar);
+        } catch (e) {
+            console.error(e);
+            return res.status(500).json({ error: e });
+        }
+    }
 }
--- src/server/entities/cellar/cellar.routes.js
+++ src/server/entities/cellar/cellar.routes.js
@@ -6,5 +6,7 @@ const router = Router();
 
 router.route('/').post(CellarController.create).get(CellarController.getDefault);
 router.route('/shared').get(CellarController.getShared);
+router.route('/list').get(CellarController.getAll);
+router.route('/:id').get(CellarController.getById);
 
 export default router;
